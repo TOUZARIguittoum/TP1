19ae16efb968203e60cef2a9f39863a0913a1b38
Amalguittoum <fa_guittoum@esi.dz>
2017-10-24 17:21:04 +0200

    test2guit_version5

--- test2guit/src/test2guit/Test2guit.java
+++ test2guit/src/test2guit/Test2guit.java
@@ -8,6 +8,7 @@ package test2guit;
  *
  * @author Admin
  */
+import static java.lang.System.exit;
 import java.util.Scanner;
 public class Test2guit {
 
@@ -77,7 +78,58 @@ public class Test2guit {
           System.out.println("le Vecteur aprés invertion :");
           v1.afficher();
           break;
-              
+          case 4:
+          System.out.println("Remplir le Vecteur:");
+          System.out.print("Donner la taille du vecteur : ");
+          taille=sc1.nextInt();
+          v1=new Vecteur(taille);
+          v1.remplir();
+          v1.retournerBornes(v);
+          System.out.println("le max est: "+v[1]+"   "+"le min est: "+v[0]);
+          break;
+          case 5:
+          System.out.println("Remplir le Vecteur:");
+          System.out.print("Donner la taille du vecteur : ");
+          taille=sc1.nextInt();
+          v1=new Vecteur(taille);
+          v1.remplir();
+          System.out.print("Donner le nombre à ajouter aux éléments de vecteur : ");
+          int nb = sc.nextInt();
+          v1.ADD(nb);
+          System.out.println("le Vecteur résultat :");
+          v1.afficher();
+          break;
+          case 6:
+          System.out.println("Remplir le Vecteur:");
+          System.out.print("Donner la taille du vecteur : ");
+          taille=sc1.nextInt();
+          v1=new Vecteur(taille);
+          v1.remplir();
+          System.out.print("Donner le nombre à soustriare des éléments du vecteur : ");
+          nb = sc.nextInt();
+          v1.SUB(nb);
+          System.out.println("le Vecteur résultat :");
+          v1.afficher();
+          break;
+          case 7:
+          System.out.println("Remplir le Vecteur:");
+          System.out.print("Donner la taille du vecteur : ");
+          taille=sc1.nextInt();
+          v1=new Vecteur(taille);
+          v1.remplir();
+          System.out.print("Donner le nombre à multiplier avec les éléments du vecteur : ");
+          nb = sc.nextInt();
+          v1.Mul(nb); 
+          System.out.println("le Vecteur résultat :");
+          v1.afficher();
+          break;
+          case 8:
+           exit(0);
+           break;
+          default:
+           System.out.println("Revoir le menu");
+           break;
+          
       
       }
           
